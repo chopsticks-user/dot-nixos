25d11260e66e22b876714f290f13ba94e44c9554
chopsticks-user <frostyfrost273@gmail.com>
2026-04-03 14:32:54 -0400

    Setup XDG directories

--- users/frost/home.nix
+++ users/frost/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, self, ... }: {
+{ config, pkgs, self, ... }: {
   home.stateVersion = self.stateVersion;
   home.username = "frost";
   home.homeDirectory = "/home/frost";
@@ -27,6 +27,27 @@
       ];
     };
   };
+  
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "${config.home.homeDirectory}/.xdg-ignore";
+    templates = "${config.home.homeDirectory}/.xdg-ignore";
+    publicShare = "${config.home.homeDirectory}/.xdg-ignore";
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/downloads";
+    pictures = "${config.home.homeDirectory}/media/images";
+    music = "${config.home.homeDirectory}/media/audio";
+    videos = "${config.home.homeDirectory}/media/videos";
+    extraConfig = {
+      XDG_MEDIA_DIR = "${config.home.homeDirectory}/media";
+      XDG_DEV_DIR = "${config.home.homeDirectory}/dev";
+      XDG_WALLPAPERS_DIR = "${config.home.homeDirectory}/media/images/wallpapers";
+      XDG_SCREENSHOTS_DIR = "${config.home.homeDirectory}/media/images/screenshots";
+      XDG_SCREENCASTS_DIR = "${config.home.homeDirectory}/media/videos/screencasts";
+    };
+  };
+
   xdg.configFile."noctalia-shell/config.json".source = ./noctalia.json;
       programs.bash = {
 	enable = true;
