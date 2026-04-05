b0c9c45f5473a81ef1d9a31c53ea39907017457a
chopsticks-user <frostyfrost273@gmail.com>
2026-04-05 13:36:48 -0400

    hyprland nvidia

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -93,6 +93,23 @@
     system.stateVersion = self.stateVersion; # Did you read the comment?
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+    boot.kernelParams = [ 
+      "nvidia-drm.modeset=1"
+      "nvidia-drm.fbdev=1"
+    ];
+    boot.initrd.kernelModules = [ 
+      "nvidia" 
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+    ];
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NVD_BACKEND = "direct";
+    };
     hardware.graphics = {
       enable = true;
     };
