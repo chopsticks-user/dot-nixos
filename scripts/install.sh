826778d7b44017d093f393e0560d6f5b0e663716
chopsticks-user <frostyfrost273@gmail.com>
2026-04-10 17:48:56 -0400

    Run git in nix-shell

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,10 +8,12 @@ username = $3
 
 # refer to flake.nix for the default password for all users
 
-git clone https://github.com/chopsticks-user/dot-nixos ~/.nixos
+nix-shell -p git --run \
+  "git clone https://github.com/chopsticks-user/dot-nixos ~/.nixos"
 
-sudo cp /mnt/etc/nixos/hardware-configuration.nix \
+sudo cp /etc/nixos/hardware-configuration.nix \
   "~/.nixos/hosts/$hostname/generated.nix"
+git -C ~/.nixos add "~/.nixos/hosts/$hostname/generated.nix"
 
 cp -r ~/.nixos/wallpapers/* ~/media/wallpapers/
 
