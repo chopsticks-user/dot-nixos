565f1d125be7907047656cb1cdb09de5e19e949e
chopsticks-user <frostyfrost273@gmail.com>
2026-04-10 17:57:36 -0400

    hostname -> host

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,18 +3,18 @@
 trap 'echo "error on line $LINENO, exiting..."; exit 1' ERR
 
 arch=$1
-hostname=$2
+host=$2
 username=$3
 
 # refer to flake.nix for the default password for all users
 
-sudo cp /etc/nixos/hardware-configuration.nix \
-  "~/.nixos/hosts/$hostname/generated.nix"
-git -C ~/.nixos add "~/.nixos/hosts/$hostname/generated.nix"
+cp /etc/nixos/hardware-configuration.nix \
+  "~/.nixos/hosts/$host/generated.nix"
+git -C ~/.nixos add "~/.nixos/hosts/$host/generated.nix"
 
 cp -r ~/.nixos/wallpapers/* ~/media/wallpapers/
 
-nh os switch -H "$hostname"
+nh os switch -H "$host"
 nh home switch -c "$username@$arch"
 
 nh clean --all
