387e05529915f441851b5cc2becff1b54741a932
chopsticks-user <frostyfrost273@gmail.com>
2026-04-10 15:58:53 -0400

    Use git during bootstrap and installation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 hostname = $1
-usb_dev = $2
 
-sudo mkdir /usb
-sudo mount "$usb_dev" /usb
-sudo cp /usb/* /etc/nixos
-sudo umount /usb
-sudo rm -rf /usb
+cd ~/.nixos
 
 sudo nix --experimental-features "nix-command flakes" \ 
   run github:nix-community/disko/latest -- --flake \
-  "/etc/nixos#$hostname" --mode destroy,format,mount
+  ".#$hostname" --mode destroy,format,mount
   
 sudo nixos-generate-config --no-filesystems --root /mnt
 cp /mnt/etc/nixos/hardware-configuration.nix \
-  "/etc/nixos/hosts/$hostname/generated.nix"
+  "./hosts/$hostname/generated.nix"
 
-sudo nixos-install --flake "/etc/nixos#$hostname" --no-root-password
+sudo nixos-install --flake ".#$hostname" --no-root-password
 
 reboot
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,16 +3,11 @@
 arch = $1
 hostname = $2
 username = $3
-usb_dev = $4
 
 # refer to flake.nix for the default password for all users
 
-sudo mkdir -p /mnt/usb
-sudo mount "$usb_dev" /mnt/usb
-mkdir ~/.nixos
-sudo -r cp /mnt/usb/* ~/.nixos/
-sudo umount /mnt/usb
-sudo rm -rf /mnt/usb
+nix-shell -p git --run \
+  "git clone https://github.com/chopsticks-user/dot-nixos ~/.nixos"
 
 sudo cp /etc/nixos/hardware-configuration.nix \
   "~/.nixos/hosts/$hostname/generated.nix"
@@ -21,6 +16,8 @@ cp -r ~/.nixos/wallpapers/* ~/media/wallpapers/
 
 nh os switch -H "$hostname"
 nh home switch -c "$username@$arch"
+
+nh clean --all
 
 reboot
 
