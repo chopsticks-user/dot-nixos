7f9a046511fb74522da98e7cf5b453f9ef5243c5
chopsticks-user <frostyfrost273@gmail.com>
2026-04-10 17:53:36 -0400

    Manual clone

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,9 +8,6 @@ username=$3
 
 # refer to flake.nix for the default password for all users
 
-nix-shell -p git --run \
-  "git clone https://github.com/chopsticks-user/dot-nixos ~/.nixos"
-
 sudo cp /etc/nixos/hardware-configuration.nix \
   "~/.nixos/hosts/$hostname/generated.nix"
 git -C ~/.nixos add "~/.nixos/hosts/$hostname/generated.nix"
