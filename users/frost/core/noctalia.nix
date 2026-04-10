9ad763ae8dd58b48518a4bd95dc757b3166744cf
chopsticks-user <frostyfrost273@gmail.com>
2026-04-10 18:12:01 -0400

    create noctalia config dir before overwriting the settings

--- users/frost/core/noctalia.nix
+++ users/frost/core/noctalia.nix
@@ -5,6 +5,7 @@
 }: {
   home.packages = with pkgs; [noctalia-shell];
   home.activation.noctalia-settings = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ~/.config/noctalia
     cp ${./noctalia.json} ~/.config/noctalia/settings.json
   '';
 }
