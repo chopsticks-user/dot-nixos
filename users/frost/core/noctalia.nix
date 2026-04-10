171ab5e6752591bc4ff73dbdddea2ce92e5dc7c6
chopsticks-user <frostyfrost273@gmail.com>
2026-04-10 18:34:45 -0400

    activation noctalia

--- users/frost/core/noctalia.nix
+++ users/frost/core/noctalia.nix
@@ -1,5 +1,12 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [noctalia-shell];
 
-  home.file.".config/noctalia/settings.json".source = ./noctalia.json;
+  home.activation.noctalia-settings = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ~/.config/noctalia
+    sudo cp ${./noctalia.json} ~/.config/noctalia/settings.json
+  '';
 }
