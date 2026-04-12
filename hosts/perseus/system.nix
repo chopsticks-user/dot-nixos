74545df0a327f3850bcf86f276b8adb410c6cd04
chopsticks-user <frostyfrost273@gmail.com>
2026-04-12 14:52:02 -0400

    Refactor perseus/system.nix

--- hosts/perseus/system.nix
+++ hosts/perseus/system.nix
@@ -3,13 +3,25 @@
   constants,
   ...
 }: {
-  system.stateVersion = "26.05";
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  features = {
+    core = {
+      enable = true;
+      kernel = "latest";
+      state-version = "26.05";
+      gpu = "nvidia";
+    };
+    grub.enable = true;
+    nh.enable = true;
+    docs.enable = true;
+    hyprland.enable = true;
+    fcitx.enable = true;
+    ssh.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    wev
+  ];
 
   networking = {
     hostName = constants.hostname;
@@ -17,43 +29,4 @@
   };
 
   time.timeZone = "America/New_York";
-
-  documentation = {
-    enable = true;
-    man = {
-      enable = true;
-      cache = {
-        enable = true;
-        generateAtRuntime = true;
-      };
-    };
-    dev.enable = true;
-    doc.enable = true;
-    info.enable = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    home-manager
-    lshw
-    fastfetch
-    wget
-    wev
-    psmisc
-    wikiman
-    tldr
-    btop-cuda
-    lazyjournal
-    ncdu
-    wl-clipboard
-    efibootmgr
-    git
-    jq
-  ];
-
-  features = {
-    fcitx.enable = true;
-    hyprland.enable = true;
-    nh.enable = true;
-    ssh.enable = true;
-  };
 }
