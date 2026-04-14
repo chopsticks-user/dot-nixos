eccd7a220fd09bf55f3ad303a8f9dfdacd36997d
chopsticks-user <frostyfrost273@gmail.com>
2026-04-14 16:49:52 -0400

    Update perseus generated hardware configs

--- hosts/perseus/generated.nix
+++ hosts/perseus/generated.nix
@@ -13,19 +13,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/33f8c86b-623d-415a-8a52-fa5759ae93a4";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D14B-5A5B";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
