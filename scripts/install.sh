c18f7d7a3a71c09362cfba32dfe2c5fceb944557
chopsticks-user <frostyfrost273@gmail.com>
2026-04-14 16:56:43 -0400

    Copy wallpapers before running nh home switch

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,8 +17,9 @@ cp /etc/nixos/hardware-configuration.nix \
   "$HOME/.nixos/hosts/$host/generated.nix"
 git add .
 
-nh home switch -c "$username@$arch"
+mkdir -p ~/media/images/wallpapers
 cp -r ~/.nixos/wallpapers/* ~/media/images/wallpapers/
+nh home switch -c "$username@$arch"
 
 nh clean all
 
