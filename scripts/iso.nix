2151b5d7bcf1b3c64ffea2c240142576a65abe9d
chopsticks-user <frostyfrost273@gmail.com>
2026-04-14 14:58:38 -0400

    Use NixOS default network interface

--- scripts/iso.nix
+++ scripts/iso.nix
@@ -3,13 +3,17 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
-  environment.etc."nixos/bootstrap.sh" = {
-    source = ./bootstrap.sh;
-    mode = "0755";
-  };
+  environment = {
+    etc = {
+      "nixos/bootstrap.sh" = {
+        source = ./bootstrap.sh;
+        mode = "0755";
+      };
 
-  environment.etc."nixos/install.sh" = {
-    source = ./install.sh;
-    mode = "0755";
+      "nixos/install.sh" = {
+        source = ./install.sh;
+        mode = "0755";
+      };
+    };
   };
 }
