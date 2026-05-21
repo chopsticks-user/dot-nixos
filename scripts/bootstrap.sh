06f01c05991526a428ec013c28f85b3ba013e4b7
chopsticks-user <frostyfrost273@gmail.com>
2026-05-21 13:03:35 -0400

    boostrap and install now accept key_cmd instead of key_path

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 host=$1
-identity_path=$2
+key_cmd=$2
 
 tmp_clone=$(mktemp -d)
 trap 'rm -rf "$tmp_clone"' EXIT
@@ -32,7 +32,7 @@ sudo nix --experimental-features "nix-command flakes" \
 system_identity=$(jq -r ".directories.system.identity" meta.json)
 mnt_system_identity="/mnt$system_identity"
 sudo mkdir -p "$(dirname "$mnt_system_identity")"
-sudo cp "$identity_path" "$mnt_system_identity"
+"$key_cmd" | sudo tee "$mnt_system_identity" > /dev/null
 sudo chmod 600 "$mnt_system_identity"
 sudo chown root:root "$mnt_system_identity"
 sudo ssh-keygen -y -f "$mnt_system_identity" \
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-identity_path=$1
+key_cmd=$1
 
 tmp_clone=$(mktemp -d)
 trap 'rm -rf "$tmp_clone"' EXIT
@@ -25,7 +25,7 @@ cd "$config_path"
 home_identity="$HOME/$(jq -r ".directories.home.identity" meta.json)"
 if [ ! -f "$home_identity" ]; then
   mkdir -p "$(dirname "$home_identity")"
-  cp "$identity_path" "$home_identity"
+  "$key_cmd" | tee "$home_identity"
   chmod 600 "$home_identity"
   ssh-keygen -y -f "$home_identity" > "$home_identity.pub"
   chmod 644 "$home_identity.pub"
@@ -37,4 +37,6 @@ home-manager switch --flake ".#$username@$arch"
 nix-collect-garbage -d
 sudo nix-collect-garbage -d
 sudo nix store optimise
+sudo rm -rf /tmp/*
+sudo rm -rf /var/tmp/*
 reboot
